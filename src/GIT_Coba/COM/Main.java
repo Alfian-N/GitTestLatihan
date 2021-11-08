ddb58a115e0b4ce5002488dc0f7ea73a9bbea77d
Alfian Naufal Rizky Ulfadi <alfiannaufal88@ittelkom-sby.ac.id>
2021-11-08 13:48:09 +0700

    change main.java

--- src/GIT_Coba/COM/Main.java
+++ src/GIT_Coba/COM/Main.java
@@ -3,6 +3,6 @@ package GIT_Coba.COM;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("The number 1 : " + new RandomNumberGen().getRandomNumber());
+	    System.out.println("The number 1 : " + new RandomNumberGen().getRandomNumber(99));
     }
 }
